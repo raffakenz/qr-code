29b539d5e93ac383f37be2cf779f0a35b98e0039
Raffa Aufa <raffaaufa69@gmail.com>
2025-05-31 16:50:09 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let imgBox = document.getElementById("imgBox")
+let qrImg = document.getElementById("qrImg")
+let qrText = document.getElementById("qrText")
+
+function generateQr() {
+    if (qrText.value.length > 0) {
+        qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value
+        imgBox.classList.add("show-img")
+    }
+}
